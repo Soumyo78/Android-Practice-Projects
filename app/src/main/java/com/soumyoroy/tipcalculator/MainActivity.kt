a0d81644d5151dc3c72246bd2e0502c9467c6dc4
Soumyo Roy <soumyo78@yahoo.com>
2020-05-23 20:51:48 +0530

    All basic functionality added

--- app/src/main/java/com/soumyoroy/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/soumyoroy/tipcalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.soumyoroy.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +11,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Creating seekbar functionality
+        seekBarTipPercent.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                seekbarPercentText.text = progress.toString()+" %"
+            }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+
+
+        // Creating a Button click function
+        calculateBtn.setOnClickListener {
+            // Getting Bill Amount
+            var billAmount = editTextBillAmount.text.toString().toInt()
+
+            // Getting tip percent
+            var tipPercent = seekBarTipPercent.progress
+
+            // Calculating tip amount
+            var tipAmount = (billAmount * tipPercent) / 100
+
+            // Calculating total payment
+            var totalPayment = billAmount + tipAmount
+
+            // Display the result
+            totalPaymentText.text = "Total Amount: ₹"+totalPayment+"\nTip Amount: ₹"+tipAmount
+        }
 
     }
 }
