3085916a7b8dfba00c005fa86ca0ebcd0fbd2cba
Soumyo Roy <soumyo78@yahoo.com>
2020-05-27 01:36:24 +0530

    App reliability improved

--- app/src/main/java/com/soumyoroy/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/soumyoroy/tipcalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.soumyoroy.tipcalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -27,9 +28,20 @@ class MainActivity : AppCompatActivity() {
         })
 
         // For No of People
+
+        // SeekBar Range
+        var MIN = 1
+
         seekBarNoOfPeople.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                seekbarNoOfPeopleText.text = progress.toString()
+                if (progress < MIN){
+                    seekBar?.progress = MIN
+                }
+
+                else if (progress >= MIN){
+                    seekbarNoOfPeopleText.text = progress.toString()
+                }
+
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
@@ -43,28 +55,33 @@ class MainActivity : AppCompatActivity() {
 
         // Creating a Button click function
         calculateBtn.setOnClickListener {
-            // Getting Bill Amount
-            var billAmount = editTextBillAmount.text.toString().toInt()
+            if (editTextBillAmount.text.isEmpty()){
+                Toast.makeText(applicationContext, "Enter the Bill Amount First", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                // Getting Bill Amount
+                var billAmount = editTextBillAmount.text.toString().toInt()
 
-            // Getting tip percent
-            var tipPercent = seekBarTipPercent.progress
+                // Getting tip percent
+                var tipPercent = seekBarTipPercent.progress
 
-            // Calculating tip amount
-            var tipAmount = (billAmount * tipPercent) / 100
+                // Calculating tip amount
+                var tipAmount = (billAmount * tipPercent) / 100
 
-            // Calculating total payment
-            var totalPayment = billAmount + tipAmount
+                // Calculating total payment
+                var totalPayment = billAmount + tipAmount
 
-            // Adding no of people
-            var noOfPeople = seekBarNoOfPeople.progress
+                // Adding no of people
+                var noOfPeople = seekBarNoOfPeople.progress
 
-            // Calculating splitted amount
-            var splittedAmount = totalPayment / noOfPeople
+                // Calculating splitted amount
+                var splittedAmount = totalPayment / noOfPeople
 
-            // Display the result
-            // totalPaymentText.text = "Total Amount: ₹"+totalPayment+"\nTip Amount: ₹"+tipAmount+"\nSplitted Amount: ₹"+splittedAmount // This is method 1 ( By using concatenating )
-            totalPaymentText.text = "Total Amount: ₹${totalPayment}\nTip Amount: ₹${tipAmount}\nSplitted Amount: ₹${splittedAmount}" // This is method 2 ( By using placeholder )
-            // We have to use dollar sign ($) in order to use placeholder
+                // Display the result
+                // totalPaymentText.text = "Total Amount: ₹"+totalPayment+"\nTip Amount: ₹"+tipAmount+"\nSplitted Amount: ₹"+splittedAmount // This is method 1 ( By using concatenating )
+                totalPaymentText.text = "Total Amount: ₹${totalPayment}\nTip Amount: ₹${tipAmount}\nSplitted Amount: ₹${splittedAmount}" // This is method 2 ( By using placeholder )
+                // We have to use dollar sign ($) in order to use placeholder
+            }
 
         }
 
